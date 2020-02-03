667c264f955a7f6a0aa3af15ade3dd7816701cd8
Nikita Kislyakov <Nikita_Kislyakov1@epam.com>
2020-02-03 23:58:06 +0400

    remove unused code

--- Sources/Domain/GatewayProtocols.swift
+++ Sources/Domain/GatewayProtocols.swift
@@ -8,12 +8,6 @@
 import Foundation
 import Combine
 
-public protocol UserPersistenceGateway {
-    func currentUser() -> AnyPublisher<User?, Error>
-    func set(_ currentUser: User) -> AnyPublisher<Void, Error>
-    func remove() -> AnyPublisher<Void, Error>
-}
-
 public protocol AuthorizationGateway {
     func authorize(credentials: Credentials) -> AnyPublisher<Tokens, Error>
 }
